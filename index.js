986161ddf8987526e547ceed927c72eea123d3fa
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-05-20 13:01:22 +0600

    Add get single toy by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -38,6 +38,13 @@ async function run() {
     //all toys
     app.get("/alltoys", async(req,res)=>{
       const result = await allToysCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/toy/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const result = await allToysCollection.findOne(filter);
       res.send(result);
     })
     // Send a ping to confirm a successful connection
